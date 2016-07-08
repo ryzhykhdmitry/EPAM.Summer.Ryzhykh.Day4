ae8446d70084e362ac50e7e925f43ef37a46d06b
Dmitry Ryzhykh <ryzhykh.dmitry@gmail.com>
2016-07-08 11:49:36 +0300

    Added Task 3 and NUnitTest

---
+++ DoubleExtension.NUnitTests/DoubleExtencionTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DoubleExtension.NUnitTests
+{
+    [TestFixture]
+    public class DoubleExtencionTests
+    {
+        [Test, TestCaseSource(typeof(MyDataClass), "TestCases")]
+        public void ConvertDouble_Minus100toBits_Bits(double number, string result)
+        {
+            string tempString;
+            tempString = number.ToBit();
+            Assert.AreEqual(result, tempString);
+        }
+    }
+
+    public class MyDataClass
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(-7111991, "1100000101011011001000010100110111000000000000000000000000000000");
+                yield return new TestCaseData(75842310111, "0100001000110001101010001000110111001111110111110000000000000000");
+                yield return new TestCaseData(double.MaxValue, "0111111111101111111111111111111111111111111111111111111111111111");
+                yield return new TestCaseData(double.NaN, "1111111111111000000000000000000000000000000000000000000000000000");                
+            }
+        }
+    }
+}
---
+++ DoubleExtension/CustomExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleExtension
+{
+    /// <summary>
+    /// Extends Double
+    /// </summary>
+    public static class CustomExtension
+    {
+        /// <summary>
+        /// Allows to represent Double value as Bit
+        /// </summary>
+        /// <param name="value">Value thta we want to convert</param>
+        /// <returns>String of bits</returns>
+        public static string ToBit(this double value)
+        {
+            StringBuilder result = new StringBuilder();
+            byte[] bytesArray = BitConverter.GetBytes(value);
+            BitArray bitsArray = new BitArray(bytesArray);
+
+            for (int i = 0; i < 64; i++)
+            {
+                if (bitsArray[i])
+                {
+                    result.Insert(0, 1);
+                }
+                else
+                {
+                    result.Insert(0, 0);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
+
